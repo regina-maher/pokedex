12ba280766f171743c7b7ce1dd053e32828b7d61
regina-maher <regina.maher8@gmail.com>
2021-12-20 14:13:13 +1100

    cleaned up the display of the items component and fixed a bug that now checks if there is a fling effect before displaying the fling effects name

--- src/components/profileComponent/statComponents/Items.jsx
+++ src/components/profileComponent/statComponents/Items.jsx
@@ -10,67 +10,54 @@ const Items = (props) => {
   }
   if (items.length > 0) {
     return (
-      <div className="Items">
+      <div className="Items  ps-4">
         <h5 className="stat-heading pb-2">items held</h5>
-        <div className="row">
-          {itemsUrlArr.map((item, index) => {
-            const { data, loading } = useFetch(item.itemHeld);
-            if (!loading) {
-              const { ...itemData } = data.data;
-              return (
-                <div className="row" key={index}>
-                  <div className="col-3">
-                    <img
-                      src={itemData.sprites.default}
-                      className="img-fluid item"
-                      alt={itemData.name}
-                    />
-                    <div className="basic-details">{itemData.name}</div>
-                    <div className="details">item id: {itemData.id}</div>
-                  </div>
-                  <div className="col-9 item-details">
-                    <ul>
-                      <li>
-                        <strong>category: </strong> {itemData.category.name}
-                      </li>
-                      <li>
-                        <strong>cost: </strong> {itemData.cost}
-                      </li>
-                      <li>
-                        {itemData.fling_effect && (
-                          <span>
-                            <strong>fling effect: </strong>
-                            {itemData.fling_effect.name}
-                          </span>
-                        )}
-                      </li>
-                      <li>
-                        {itemData.fling_power && (
-                          <span>
-                            <strong>fling power: </strong>
-                            {itemData.fling_power}
-                          </span>
-                        )}
-                      </li>
-                      <li>
-                        {itemData.baby_trigger_for && (
-                          <span>
-                            <strong> baby trigger:</strong>
-                            {itemData.baby_trigger_for}
-                          </span>
-                        )}
-                      </li>
-                      <li>
-                        {itemData.effect_entries[0].effect ??
-                          itemData.effect_entries[0].effect}
-                      </li>
-                    </ul>
-                  </div>
+        {itemsUrlArr.map((item, index) => {
+          const { data, loading } = useFetch(item.itemHeld);
+          if (!loading) {
+            const { ...itemData } = data.data;
+            console.log(itemData);
+            return (
+              <div className="row" key={index}>
+                <div className="col-3">
+                  <img
+                    src={itemData.sprites.default}
+                    className="img-fluid item"
+                    alt={itemData.name}
+                  />
+                  <div className="basic-details">{itemData.name}</div>
+                  <div className="details">item id: {itemData.id}</div>
                 </div>
-              );
-            }
-          })}
-        </div>
+                <div className="col-9 item-details">
+                  <div className="row">
+                    <div className="col-4 item-titles">
+                      <ul>
+                        <li>category:</li>
+                        <li>cost:</li>
+                        <li>fling effect:</li>
+                        <li>fling power:</li>
+                        <li>baby trigger:</li>
+                      </ul>
+                    </div>
+                    <div className="col-8 item-values">
+                      <ul>
+                        <li>{itemData.category.name}</li>
+                        <li>{itemData.cost}</li>
+                        <li>{itemData.fling_effect?.name || "n/a"}</li>
+                        <li>{itemData.fling_power || "n/a"}</li>
+                        <li>{itemData.baby_trigger_for || "no"}</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="item-description pb-3">
+                    {itemData.effect_entries[0].effect || "no description"}
+                  </div>
+                  <hr />
+                </div>
+              </div>
+            );
+          }
+        })}
       </div>
     );
   } else {
