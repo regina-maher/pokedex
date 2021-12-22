b04d70986189ce38f617252385faa8a246afedae
regina-maher <regina.maher8@gmail.com>
2021-12-22 19:00:23 +1100

    updated the localStorage effect to check if there is data in the local host so it doesnt load null on load

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,23 @@ import { Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [results, setResults] = useState(() =>
-    JSON.parse(localStorage.getItem("results") || "")
-  );
+  const [results, setResults] = useState("");
+  // const [results, setResults] = useState(() =>
+  //   JSON.parse(localStorage.getItem("results"))
+  // );
+  // useEffect(() => {
+  //   localStorage.setItem("results", JSON.stringify(results));
+  // }, [results]);
+  const LOCAL_STORAGE_KEY = "results";
   useEffect(() => {
-    localStorage.setItem("results", JSON.stringify(results));
+    const storageResults = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageResults) {
+      setTodos(storageResults);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(results));
   }, [results]);
   return (
     <div className="App">
