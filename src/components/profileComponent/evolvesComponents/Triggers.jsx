bfc12b319e3f35fd2e385dd03c9d4afdf5566fde
regina-maher <regina.maher8@gmail.com>
2022-01-08 15:49:54 +1100

    removed console.logs

--- src/components/profileComponent/evolvesComponents/Triggers.jsx
+++ src/components/profileComponent/evolvesComponents/Triggers.jsx
@@ -17,7 +17,6 @@ const Triggers = (props) => {
     return (
       <div className="Triggers">
         {summay.map((detail, index) => {
-          console.log(detail[0], detail[1]);
           return (
             <div className="d-flex" key={index}>
               <div className="details key col-7">
